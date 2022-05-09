4ac4a3b0a500e331420a5cb972a4df9008aa8cc6
Esmeralda Santoyo <58286718+EsmeSantoyo@users.noreply.github.com>
2022-05-09 13:03:43 -0500

    Add: Creando server con API

---
+++ fiz_buzz/lib/server.js
@@ -0,0 +1,31 @@
+const ExplorerController = require("./controllers/ExplorerController");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.json({message: "FizzBuzz Api welcome!"});
+});
+
+app.get("/v1/explorers/:mission", (request, response) => {
+  const mission = request.params.mission;
+  const explorerInMission = ExplorerController.getExplorersByMission(mission);
+  response.json(explorerInMission);
+});
+
+app.get("/v1/explorers/amount/:mission", (request, response) => {
+  const mission = request.params.mission;
+  const explorersAmountInMission = ExplorerController.getExplorersAmonutByMission(mission);
+  response.json({mission: request.params.mission, quantity: explorersAmountInMission});
+});
+
+app.get("/v1/explorers/usernames/:mission", (request, response) => {
+  const mission = request.params.mission;
+  const explorerUsernameInMission = ExplorerController.getExplorersUsernamesByMission(mission);
+  response.json(explorerUsernameInMission);
+});
+
+app.listen(port, () => {
+  console.log(`FizzBuzz API in localhost:${port}`);
+});
